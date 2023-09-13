3c28e425258bab0dd4806a594269141e9ba6e892
mariaprang <marie.prangishvili@gmail.com>
2023-09-13 18:58:14 +0400

    Initial commit

---
+++ src/test/java/com/example/springtacocloud/SpringTacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
